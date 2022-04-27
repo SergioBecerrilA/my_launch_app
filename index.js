ce14b3746cd750264b339ebeacb18ccd39c4e2c7
Sergio Enrique Becerril Alejandre <99156388+SergioBecerrilA@users.noreply.github.com>
2022-04-27 11:14:43 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const sergio = new MissionCommander("Sergio")
+const luis = new MissionCommander("Luis")
+
+console.log(carlo.name)
+console.log(sergio.name)
+console.log(luis.name)
